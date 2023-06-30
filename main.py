53d2fdbe9c45fe5a6fe677713ee31fbb1166a8b9
Brendon <54781242+almeida25@users.noreply.github.com>
2023-06-30 20:08:40 -0300

    nova versão

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import streamlit         as st
 import pandas            as pd
 import numpy             as np
@@ -9,21 +10,6 @@ import plotly.graph_objs as go
 from st_aggrid        import AgGrid, GridOptionsBuilder
 
 PRIMARY_COLOR = "#572b52"
-
-def main():
-    st.header("Projeto da Vitivinicultura")
-    st.write(" Apresentamos um projeto de análise da importação e exportação da Vitivinicultura como uma oportunidade estratégica de investimento. A indústria vinícola, com seu potencial de crescimento e lucratividade, tem se mostrado cada vez mais atrativa para investidores interessados em explorar os mercados internacionais de vinho.")
-    st.write("Uma das principais vantagens competitivas desta vinícola é a sua capacidade de combinar tradição e inovação. Com uma equipe de enólogos experientes, que valorizam as técnicas tradicionais de vinificação, mas também estão abertos à adoção de métodos inovadores, a vinícola tem conseguido criar vinhos que encantam paladares exigentes em todo o mundo. Além disso, a vinícola tem uma sólida estratégia de comércio e produção para sustentar suas atividades de importação e exportação. A produção é cuidadosamente planejada, levando em consideração as demandas e tendências do mercado global de vinhos.")
-    st.write("No que se refere ao comércio, a empresa busca estabelecer parcerias estratégicas com distribuidores e importadores em mercados-chave ao redor do mundo. Essas parcerias permitem a ampla distribuição dos vinhos da vinícola em diferentes regiões, atingindo consumidores exigentes e explorando novas oportunidades de negócio.")
-
-
-    st.header("Objetivo do Projeto")
-    st.write("O objetivo é demonstrar análises abrangentes da importação e exportação da Vitivinicultura, com foco em fornecer informações relevantes para investidores interessados em explorar as oportunidades de negócios nesse setor. Buscamos identificar as principais tendências, desafios e oportunidades do comércio internacional de vinhos, analisando as dinâmicas globais do mercado e distribuição.")
-    st.write("Neste contexto, o presente projeto tem como objetivo analisar também detalhadamente o comércio e a produção da vinícola no âmbito da importação e exportação de vinhos. Ao realizar essa análise aprofundada da importação e exportação de vinhos do Brasil, esperamos contribuir para o entendimento e a valorização dessa indústria, bem como fornecer insights para aqueles que desejam se envolver nesse mercado em expansão, também espera-se oferecer aos investidores uma visão clara e abrangente sobre a vinícola, destacando as vantagens competitivas, o potencial de crescimento e as oportunidades de investimento no setor. Essas informações serão fundamentais para embasar decisões estratégicas e maximizar o retorno sobre o investimento.")
-
-
-if __name__ == '__main__':
-    main()
 
 def apply_custom_style():
     st_custom_style = """
@@ -180,7 +166,7 @@ with st.expander(" Maiores Exportadores de Vinho"):
     cols = df_exp_vinho_maiores_lucros.columns[1::2]
     df_exp_vinho_maiores_lucros = df_exp_vinho_maiores_lucros[cols]
     #st.write(df_exp_vinho_maiores_lucros)
-    df_sample = df_exp_vinho_maiores_lucros.head(10)
+    df_sample = df_exp_vinho_maiores_lucros.head(15)
     df_sample.drop(columns='Total em US$', inplace=True)
     df_sample = df_sample.T
     fig = go.Figure()
@@ -194,12 +180,14 @@ with st.expander(" Maiores Exportadores de Vinho"):
     
 with st.expander("Exportações e Lucros do Brasil nos Últimos 15 anos"):
     st.subheader("Linha do Tempo dos Países que mais Exportaram e Geraram Lucro ao Brasil nos Últimos 15 Anos")
-    v0 = df_exp_vinho_litros[df_exp_vinho_litros.columns[1::2][:-1]].sum().values.tolist()
-    v1 = df_exp_vinho_litros[df_exp_vinho_litros.columns[0::2][:-1]].sum().values.tolist()
+    df_exp_vinho_litros1 = df_exp_vinho_litros.sort_values(by='Total em US$', ascending=False)
+    df_exp_vinho_litros1 = df_exp_vinho_litros1.head(15)
+    v0 = df_exp_vinho_litros1[df_exp_vinho_litros1.columns[1::2][:-1]].sum().values.tolist()
+    v1 = df_exp_vinho_litros1[df_exp_vinho_litros1.columns[0::2][:-1]].sum().values.tolist()
 
     fig = go.Figure(data=[
-        go.Bar(name="Total em Litros(Kg)", x=df_exp_vinho_litros.columns[0::2][:-1], y=v0, marker_color='indianred'),
-        go.Bar(name="Total em US$", x=df_exp_vinho_litros.columns[0::2][:-1], y=v1, marker_color='lightsalmon')
+        go.Bar(name="Total em US$", x=df_exp_vinho_litros.columns[0::2][:-1], y=v0, marker_color='indianred'),
+        go.Bar(name="Total em Litros(Kg)", x=df_exp_vinho_litros.columns[0::2][:-1], y=v1, marker_color='lightsalmon')
     ])
     
     #fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
@@ -210,6 +198,55 @@ with st.expander("Exportações e Lucros do Brasil nos Últimos 15 anos"):
       dtick = 1
    ))
     st.plotly_chart(fig)
+    
+with st.expander("Grafico de Mapa"):
+    df_exp_vinho_litros_mapa = df_exp_vinho_litros.copy()
+    df_exp_vinho_litros_mapa = df_exp_vinho_litros_mapa[df_exp_vinho_litros_mapa.columns[0::2][:-1]]
+    
+    paises =  pd.read_json('assets/iso_paises.json', lines=True)
+    df_exp_vinho_litros_mapa['ISO ALPHA'] = ''
+    for _, row in paises.iterrows():
+        df_exp_vinho_litros_mapa.loc[df_exp_vinho_litros_mapa.index.str.contains(row['nome']), 'ISO ALPHA'] = row['alpha-3']
+    
+    st.write(df_exp_vinho_litros_mapa.loc[df_exp_vinho_litros_mapa['ISO ALPHA'] == ''])
+    
+    pais  = []
+    ano   = []
+    valor = []
+    iso   = []
+    for pais_name, row  in df_exp_vinho_litros_mapa.iterrows():
+        ano_start = 2007
+        for _ in range(0, 15):
+            pais.append(pais_name)
+            ano.append(ano_start)
+            valor.append(row[f'{ano_start}'])
+            iso.append(row['ISO ALPHA'])
+            ano_start += 1
+
+    df_mapa = pd.DataFrame({"País": pais, "Ano": ano, "Litros(KG)": valor, "ISO Alpha": iso})
+    st.subheader("Litros Exportados nos Últimos 15 anos")
+    fig = px.choropleth(df_mapa, locations='ISO Alpha', color='Litros(KG)', hover_name='País',
+                        projection='eckert4', animation_frame='Ano',color_continuous_scale=[[0, 'rgb(240,240,240)'],
+                      [0, 'rgb(255,255,255)'],
+                      [0.25, 'rgb(161,119,155)'],
+                      [0.5, 'rgb(139,95,133)'],
+                      [0.75, 'rgb(107,62,101)'],
+                      [1, 'rgb(87,43, 82)']],
+                        title='GDP per Capita by Country')
+    
+    
+    fig.update_layout(geo=dict(bgcolor= 'rgba(0,0,0,0)', lakecolor='#4E5D6C',
+                                          landcolor='rgba(51,17,0,0.2)',
+                                          subunitcolor='grey'),
+                                  title = '',font = {"size": 13, "color":"White"},titlefont = {"size": 25, "color":"White"},
+                                  geo_scope='world', margin={"r":0,"t":40,"l":0,"b":0}, plot_bgcolor='#4E5D6C', 
+                                  width=1300, height=600)
+    st.plotly_chart(fig)
+    
+
+with st.expander("$ Vendido"):
+    st.write('test')
+    
     # plt.figure(figsize=(22,8))
     # plt.plot(df_sample.T.index, df_sample.T.values)
     # plt.legend(df_sample.T.columns[0:12])
@@ -233,3 +270,4 @@ with st.expander("Exportações e Lucros do Brasil nos Últimos 15 anos"):
     # #plt.axhline(y=df_exportacao_geral_litros.sum().mean(), color='blue', linestyle='--', linewidth=3, label='Média')
     # plt.title("Linha do Tempo de Exportação de Vinho e Lucro(US$) do Brasil nos Últimos 15 anos")
     # plt.legend();
+    
